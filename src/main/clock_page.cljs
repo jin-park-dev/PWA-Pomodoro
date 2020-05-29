dd63383c53216adaa5c571e69b71d2c85c09dea8
Jin Park <jinpark.han@gmail.com>
2020-05-29 18:07:14 +0100

    Basic clock working. Making more styled clock.

--- src/main/clock_page.cljs
+++ src/main/clock_page.cljs
@@ -3,21 +3,51 @@
    [reagent.core :as reagent]
    [date-fns :as date-fns]))
 
-(defn clock []
-  (let [time-now (reagent/atom (.now js/Date))
-        state (reagent/atom {})
-        ]
+; Turns out reagent has it's own way of doing it regarding issue with assigning variable and destruction that's not done JS way (and form3 isn't requried)
+; Replaces, "create-class :component-did-mount / :component-will-unmount pattern" - https://www.reddit.com/r/Clojurescript/comments/5htkbc/how_to_use_the_withlet_macro_in_reagent_060/
+#_(defn clock-form3 []
+  (reagent/with-let [time-now (reagent/atom (.now js/Date))
+                     state (reagent/atom {})]
     (reagent/create-class
      {:display-name "clock component"
       :component-did-mount (fn [] (js/setInterval #(reset! time-now (.now js/Date)) 500))  ; every 500ms (1/2 second) to be more accurate.
       :component-did-update (fn [] (println "clock - component-did-update"))
-      :component-will-unmount (fn [] (println "clock - component-will-unmount"))
+      :component-will-unmount (fn [] (js/clearInterval time-now))
 
-      :reagent-render (fn [] [:div#clock 
+      :reagent-render (fn [] [:div#clock
+                              {:on-click (fn [] (js/clearInterval time-now))}
                               [:div "Date: " (date-fns/format @time-now "MM/dd/yyyy")]
-                              [:div "Time: " (date-fns/format @time-now "h m s aaa")]
-                              ])}))
-  )
+                              [:div "Time: " (date-fns/format @time-now "h:mm:ss aaa")]])})))
+
+; with-let seems only need form-1 or is it form-2 ??? - https://github.com/reagent-project/reagent/issues/378
+; Found with-let here - https://stackoverflow.com/questions/30280484/making-a-simple-countdown-timer-with-clojure-reagent
+(defn clock-simple []
+  (reagent/with-let [time-now (reagent/atom (.now js/Date))
+                     timer-fn (js/setInterval #(reset! time-now (.now js/Date)) 500)
+                     state (reagent/atom {:currently-unused ""})]
+
+    [:div#clock-simple
+     {:on-click (fn [] (println "clicked clock-simple"))}
+     [:div "Date: " (date-fns/format @time-now "MM/dd/yyyy")]
+     [:div "Time: " (date-fns/format @time-now "h:mm:ss aaa")]]
+                    
+    (finally (js/clearInterval timer-fn))))
+
+(defn clock-styled []
+  (reagent/with-let [time-now (reagent/atom (.now js/Date))
+                     timer-fn (js/setInterval #(reset! time-now (.now js/Date)) 500)
+                     state (reagent/atom {:currently-unused ""})]
+
+    [:div#clock-styled.flex.flex-row
+     [:div (date-fns/format @time-now "h")]
+     [:div ":"]
+     [:div (date-fns/format @time-now "mm")]
+     [:div ":"]
+     [:div.mr-1 (date-fns/format @time-now "ss")]
+     [:div (date-fns/format @time-now "aaa")]
+     ]
+                    
+    (finally (js/clearInterval timer-fn))))
 
 (defn counting-button-in-form3 [txt]
   (let [state (reagent/atom 0)]
@@ -33,10 +63,10 @@
 (defn clock-panel []
   [:div
    [:h1 "clock panel"]
-   [clock]
-  ;  [counting-button-in-form3]
-
-  ;  [:br]
+  ;  [:h2 "Clock Simple"]
+  ;  [clock-simple]
+   [:h2 "Clock Styled"]
+   [clock-styled]
   ;  (date-fns/format (.getTime (js/Date.)) "MM/dd/yyyy")
   ;  [:br]
   ;  (date-fns/format (.now js/Date) "MM/dd/yyyy")
--- src/main/pomodoro_page.cljs
+++ src/main/pomodoro_page.cljs
@@ -4,14 +4,21 @@
 
 
 
+(defn countdown-component []
+  (reagent/with-let [seconds-left (reagent/atom 60)
+                     timer-fn     (js/setInterval #(swap! seconds-left dec) 1000)]
+    [:div.timer
+     [:div "Time Remaining: " (str @seconds-left)]]
+    (finally (js/clearInterval timer-fn))))
+
 (defn pomodoro-panel []
   [:div
    [:h1 "pomodoro panel"]
-   [:div
-    ]
+   [:h2 "countdown-component"]
+
+   [countdown-component]
    [:div]
-   [:div]
-   ])
+   [:div]])
 
 (defn pomodoro-page-container []
   [:div
