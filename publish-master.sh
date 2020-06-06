cee60819882b388a6aba6bc1853bab625d6e7b3c
Jin Park <jinpark.han@gmail.com>
2020-06-06 22:38:47 +0100

    Remove confirmation of pushing (as not writing over database in this case) for server deployment

--- publish-master.sh
+++ publish-master.sh
@@ -6,14 +6,11 @@ LOCAL_DEV=${PWD}
 echo "Must run from base directory"
 echo "Running publish-master.sh"
 echo "Publishing to 'pomo.kanxdoro.com' - ME =======> SERVER (Seb)"
-read -p "Are you sure? (y/n) " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo "===============rsync-ing files==================="
-    rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group public/ seb:/home/jin/sites/pomo.kanxdoro.com
-    # Why below not working?
-    # rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group $LOCAL_DEV/public $USER_ME@$TARGET 
-    echo "================================="
-    echo "Uploaded to " $TARGET
-    echo "================================="
-fi
+echo "===============rsync-ing files==================="
+rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group public/ seb:/home/jin/sites/pomo.kanxdoro.com
+# Why below not working?
+# rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group $LOCAL_DEV/public $USER_ME@$TARGET 
+echo "================================="
+echo "Uploaded to " $TARGET
+echo "================================="
+
