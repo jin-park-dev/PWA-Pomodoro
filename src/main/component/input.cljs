a69ea0deb3bb9f20e009b1837e6bb73a7411cff3
Jin Park <jinpark.han@gmail.com>
2020-06-07 12:32:33 +0100

    Add tooltip

--- src/main/component/input.cljs
+++ src/main/component/input.cljs
@@ -11,14 +11,27 @@
    "
   [{:keys [value handle-change handle+change class]
     :or   {value 0  ; default if no number
+        ;;    
            handle-change (fn [] (js/console.log "handle-change pressed. Please add function"))
            handle+change (fn [] (js/console.log "handle+change pressed. Please add function"))
-           class nil}
-    }]
+           class nil}}]
   (let []
     [:div.flex.flex-row {:class class}
-     [:button#break-decrement.btn.btn-nav {:on-click handle-change}
-      "-"]
-     [:div#break-length.btn.btn-nav.mx-4 value]
-     [:button#break-increment.btn.btn-nav {:on-click handle+change}
-      "+"]]))
+     [:button#break-decrement.btn.btn-nav {:on-click handle-change} "-"]
+     [:div#break-length.btn.btn-nav.clean-number-input.cursor-default.mx-4 value]
+     [:button#break-increment.btn.btn-nav {:on-click handle+change} "+"]]))
+
+
+
+     
+;;   TODO: Very tempted to make it input but it requires custom validation for input to behave nice. Otherwise Right now we can add letters, etc
+;;   :handle-value-change (fn [e] (swap! state assoc-in [:value-break] (-> e .-target .-value)))
+;;   handle-value-change (fn [] (js/console.log "handle-value-change pressed. Please add function"))
+
+#_[:div#break-length.btn.btn-nav.clean-number-input.mx-4
+   {:type "number"
+    :min min
+    :max max
+    :pattern "[0-9]+"
+    :value value
+    :on-change handle-value-change}]
--- src/main/pomodoro_page.cljs
+++ src/main/pomodoro_page.cljs
@@ -5,6 +5,7 @@
    [state.subs :as sub] ; Needed for subs
    [stylefy.core :as stylefy :refer [use-style]]
    [date-fns :as date-fns]
+   ["react-tooltip" :as ReactTooltip]
    [util.time :refer [seconds->duration]]
    [util.dev :refer [dev-panel]]
    [component.clock :as clock]
@@ -69,9 +70,9 @@
 
                                           :start? true
                                           :finished? false
-                                          
+
                                           :value-break 5  ;default break of 5 minutes
-                                          
+
                                           :dev? (rf/subscribe [:dev?])}) ; Seems not reactive if I destructure here
                      timer-fn     (js/setInterval
                                    (fn []
@@ -88,16 +89,21 @@
                               (get-in @state [:start?]) compound-duration-plus-ms
                               :else {:h 0 :m 0 :s 0 :ms 0})]
       [:div.flex.flex-col.items-center.justify-center.content-center.self-center
-               [:div#break-label.btn.hidden "Break Length"] ; HIDDEN. Here for Freecodecamp requirement
-       [input/number {:value (get-in @state [:value-break])
-                      :handle-change (fn [] (swap! state update-in [:value-break] dec))
-                      :handle+change (fn [] (swap! state update-in [:value-break] inc))
-                      :class "transition-25to100 mb-10"}]
-       [:div.flex.flex-row 
+       [:div#break-label.btn.hidden "Break Length"] ; HIDDEN. Here for Freecodecamp requirement
+       [:div {:data-tip "Break Length"}
+        [input/number {:value (get-in @state [:value-break])
+                       :handle-change (fn [] (swap! state update-in [:value-break] dec))
+                       :handle+change (fn [] (swap! state update-in [:value-break] inc))
+                       :class "transition-25to100 mb-10"}]
+        [:> ReactTooltip {:place "top"  ; This tooltip can be place anywhere. 
+                          :type "light"  ;dark is default
+                          :effect "solid"}]]
+
+       [:div.flex.flex-row
         [:div#timer-label.btn.hidden "Session"] ; HIDDEN. Here for Freecodecamp requirement
         [:div#session-length.btn.hidden "25"] ; TODO: Get value from state     HIDDEN. Here for Freecodecamp requirement
         [:div#time-left.btn.hidden "25:00 (mm:ss format)"] ; TODO: Get value from state     HIDDEN. Here for Freecodecamp requirement
-        
+
         [:button#session-decrement.btn.btn-nav.rounded-l-full.rounded-r.self-center.transition-25to100 "-"]
         [:div.flex.flex-row.text-6xl.tracking-wide.leading-none.text-teal-500.text-opacity-100.cursor-pointer.select-none.mx-12
          {:on-click #(swap! state update-in [:ms-visible?] not)}
@@ -113,14 +119,14 @@
                                                      (swap! state assoc-in [:now] (date-fns/addMinutes (.now js/Date) 25)))}
          "Reset"]
         [:button#start_stop.btn.btn-nav {:on-click (fn [e]
-                                          (swap! state update-in [:start?] not)
-                                          (swap! state assoc-in [:finished?] false)
-                                          (swap! state assoc-in [:start] (date-fns/addMinutes (.now js/Date) 25))
-                                          (swap! state assoc-in [:now] (date-fns/addMinutes (.now js/Date) 25)))}
+                                                     (swap! state update-in [:start?] not)
+                                                     (swap! state assoc-in [:finished?] false)
+                                                     (swap! state assoc-in [:start] (date-fns/addMinutes (.now js/Date) 25))
+                                                     (swap! state assoc-in [:now] (date-fns/addMinutes (.now js/Date) 25)))}
          (if (get-in @state [:start?]) "Stop" "Start")]]
-        #_[:button.btn.btn-nav.mt-2 {:on-click (fn [e]
-                                                 (rf/dispatch [:dev/dev-switch]))}
-           "dev: " (pr-str @(rf/subscribe [:dev?]))]
+       #_[:button.btn.btn-nav.mt-2 {:on-click (fn [e]
+                                                (rf/dispatch [:dev/dev-switch]))}
+          "dev: " (pr-str @(rf/subscribe [:dev?]))]
        (when @(get-in @state [:dev?]) [dev-panel [state]])])
     (finally (js/clearInterval timer-fn))))
 
@@ -128,7 +134,7 @@
   (let [nav-styled? (reagent/atom true)]
     (fn []
       [:div.flex-center.h-full
-       [:div.mb-5 (if @nav-styled? [:button.btn.btn-nav {:on-click #(swap! nav-styled? not)} "Adv"] [:button.btn.btn-nav {:on-click #(swap! nav-styled? not)} "Clean"])]
+       [:div.mb-5 (if @nav-styled? [:button.btn.btn-nav {:on-click #(swap! nav-styled? not)} "adv"] [:button.btn.btn-nav {:on-click #(swap! nav-styled? not)} "Clean"])]
        [:div.flex-center.h-full.w-full (if @nav-styled? [pomodoro-simple--options] [:div [pomodoro-simple]])]])))
 
 (defn pomodoro-page-container []
