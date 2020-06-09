0f25dfd29c09ad3b8583ab2eeea0657e47fe3cee
Jin Park <jinpark.han@gmail.com>
2020-06-09 19:16:21 +0100

    For Deployment

--- src/main/app.cljs
+++ src/main/app.cljs
@@ -74,6 +74,6 @@
   ; Add other initalization here if needed such as ajax call for inital data
   (rf/dispatch-sync [::events/initialize-db])  ; Difference with dispatch is this is synchronous (unlike dispatch which is asynchronous). Guarantees this line is done before next.
   (rf/dispatch-sync [::events/initialize-db-user-settings]) ; Default user settings
-  (rf/dispatch-sync [::events/initialize-db-dev]) ; Inital dev setting
+  ;; (rf/dispatch-sync [::events/initialize-db-dev]) ; Inital dev setting
   (start) ; Reagent (react)
   (stylefy/init))
