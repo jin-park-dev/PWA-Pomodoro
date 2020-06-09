24c586262fc2b2bdc01f04ae4ef883b8e6c9aeb0
Jin Park <jinpark.han@gmail.com>
2020-06-09 19:53:22 +0100

    For Deployment

--- publish-master.sh
+++ publish-master.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-USER_ME = 'jin'  # $USER on Mac
-TARGET='seb:/home/jin/sites/pomo.kanxdoro.com'
+USER_ME='jin'  # $USER on Mac
+TARGET='51.15.104.153:/home/jin/sites/pomo.kanxdoro.com'
 LOCAL_DEV=${PWD}
 echo "Must run from base directory"
 echo "Running publish-master.sh"
 echo "Publishing to 'pomo.kanxdoro.com' - ME =======> SERVER (Seb)"
 echo "===============rsync-ing files==================="
-rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group public/ seb:/home/jin/sites/pomo.kanxdoro.com
+rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group public/ $USER_ME@$TARGET
 # Why below not working?
 # rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group $LOCAL_DEV/public $USER_ME@$TARGET 
 echo "================================="
