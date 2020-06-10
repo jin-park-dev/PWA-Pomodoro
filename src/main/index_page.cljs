1b87ec4a9cbc341605ea04fe9b8203b6e9f1868c
Jin Park <jinpark.han@gmail.com>
2020-06-10 13:33:10 +0100

    Add app state (Page based). Add animation to index page for initial visit

--- src/main/index_page.cljs
+++ src/main/index_page.cljs
@@ -1,37 +1,59 @@
 (ns index-page
   (:require
    [reagent.core :as reagent]
-   [stylefy.core :as stylefy :refer [use-style]]))
+   [re-frame.core :as rf]
+   [util.dev :refer [dev-panel]]
+   [stylefy.core :as stylefy :refer [use-style]]
+   [component.style :refer [fn-animate-css]]
+   [state.index]))
 
 ; reagent doc - https://reagent-project.github.io/docs/master/reagent.core.html#var-create-class
 ; stylefy doc - https://jarzka.github.io/stylefy/doc/stylefy.core.html
 
+;; stylefy tryout
+;; 
+;; (def button-style {:padding "25px"
+;;                    :background-color "#BBBBBB"
+;;                    :border "1px solid black"})
 
-(def button-style {:padding "25px"
-                   :background-color "#BBBBBB"
-                   :border "1px solid black"})
+;; (defn animate-css-flip-in
+;;   "Used for into title on Index Page"
+;;   [css-atom]
+;;   (fn-animate-css css-atom "animate__flipInX" nil 5000))
 
-(defn- button-none-stylefy [text]
-  [:div {:on-click #(.log js/console "Click! button-none-stylefy")
-         :style {:height 55
-                 :width 200
-                 :border "solid"}
-         :class "some-3rd-party-button-class"}
-   text])
 
-(defn- button-stylefy [text]
-  [:div (use-style button-style {:on-click #(.log js/console "Click! button-stylefy")
-                                 :class "some-3rd-party-button-class"})
-   text])
+;; (defn- button-none-stylefy [text]
+;;   [:div {:on-click #(.log js/console "Click! button-none-stylefy")
+;;          :style {:height 55
+;;                  :width 200
+;;                  :border "solid"}
+;;          :class "some-3rd-party-button-class"}
+;;    text])
+
+;; (defn- button-stylefy [text]
+;;   [:div (use-style button-style {:on-click #(.log js/console "Click! button-stylefy")
+;;                                  :class "some-3rd-party-button-class"})
+;;    text])
+
 
 (defn index-panel []
-  [:div.flex-center.h-full
-   [:h1.animate__animated.animate__flipInX "Welcome to PWAdoro"]
-   [:p "Simple no nonsense Pomodoro Timer"]
-   [:p "Pomodoro Timer"]])
+  (let [css-h1-intro (if @(rf/subscribe [:index-initial?])
+                       (reagent/atom "animate__flipInX")
+                       (reagent/atom nil)  ; ;or empty string better? ""
+                       )]
+    (fn []
+      ;; (fn-animate-css css-h1-intro "animate__flipInX" nil 5000)
+      [:div.flex-center.h-full
+       [:h1.cursor-pointer.select-none.animate__animated {:class @css-h1-intro
+                                                          :on-click (fn [] (fn-animate-css css-h1-intro "animate__flipInX" nil 1200))}
+        "Welcome to PWAdoro"]
+       [:p "Simple no nonsense Pomodoro Timer"]
+       (when @(rf/subscribe [:dev?]) [dev-panel [css-h1-intro]])])))
 
 (defn index-page-container []
-  [:main [index-panel]])
+  (reagent/with-let []
+    [:main [index-panel]]
+    (finally (rf/dispatch [:user-initial-visit false]))))
 
 
 ;; [:footer "The footer"]
---
+++ src/main/state/index.cljs
@@ -0,0 +1,18 @@
+(ns state.index
+  (:require
+   [re-frame.core :as rf]
+   #_[state.db :as db]))
+
+; Trying different way of keeping event and subscription together. Based on page over events
+
+
+(rf/reg-event-db
+ ;;  "Set initial? to true or false"
+ :user-initial-visit
+ (fn [db [_ true-or-false]]
+   (assoc-in db [:ui :index :initial?] true-or-false)))
+
+(rf/reg-sub
+ :index-initial?
+ (fn [db]
+   (get-in db [:ui :index :initial?])))
