1edbf0e803c6d84faff48ae84c46cea5d4c1a175
Jin Park <jinpark.han@gmail.com>
2020-06-11 00:32:42 +0100

    Add Express server to test build. Modify deploy script to match new public folder

--- public/sw.js
+++ public/sw.js
@@ -3,26 +3,32 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox
 if (workbox) {
   console.log(`Workbox is loaded`);
 
-  workbox.core.skipWaiting();
-  // workbox.precaching.precacheAndRoute([]);
-  // workbox.precaching.precacheAndRoute([
-  //   {url: '/index.html', revision: '1' },
-  // ]);
-  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+  // workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+  // workbox.precaching.precacheAndRoute([]); 
+  workbox.precaching.precacheAndRoute([
+    {url: '/', revision: '1'},
+    {url: '/index.html', revision: '1'},
+    {url: '/clock', revision: '1'},
+    {url: '/timer', revision: '1'},
+    {url: '/pomodoro', revision: '1'},
+  ]);
+
+
+  
 
 //   cacheFirst
-//   workbox.routing.registerRoute(
-//     /(.*)articles(.*)\.(?:png|gif|jpg)/,
-//     workbox.strategies.cacheFirst({
-//       cacheName: 'images-cache',
-//       plugins: [
-//         new workbox.expiration.Plugin({
-//           maxEntries: 50,
-//           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-//         })
-//       ]
-//     })
-//   );
+  workbox.routing.registerRoute(
+    /(.*)articles(.*)\.(?:png|gif|jpg)/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'images-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        })
+      ]
+    })
+  );
 
 //   networkFirst
 //   const articleHandler = workbox.strategies.networkFirst({
--- publish-master.sh
+++ publish-master.sh
@@ -7,7 +7,7 @@ echo "Must run from base directory"
 echo "Running publish-master.sh"
 echo "Publishing to 'pomo.kanxdoro.com' - ME =======> SERVER (Seb)"
 echo "===============rsync-ing files==================="
-rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group public/ $USER_ME@$TARGET
+rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group build/ $USER_ME@$TARGET
 # Why below not working?
 # rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group $LOCAL_DEV/public $USER_ME@$TARGET 
 echo "================================="
